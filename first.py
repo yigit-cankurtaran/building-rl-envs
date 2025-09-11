74fe823a9540a828104d6fdeb6a83fdfde41b03d
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-11 19:38:34 +0300

    barebones env

--- first.py
+++ first.py
@@ -12,3 +12,34 @@ class CustomEnv(gym.Env):
         self.observation_space = spaces.Box(
             low=0, high=255, shape=(84, 84, 3), dtype=np.uint8
         )
+
+        self.state = None
+        self.steps = 0
+        self.max_steps = 1000
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)  # calls reset from gym.Env, not itself
+
+        self.state = self.observation_space.sample()
+        self.steps = 0
+        info = {}
+        return self.state, info
+
+    def step(self, action):
+        # execute action, return (obs,reward,term,trunc,info)
+
+        self.steps += 1
+
+        # TODO: fill these out
+        next_state = self.observation_space.sample()  # placeholder
+        reward = np.random.random()  # placeholder
+        terminated = False  # episode ends when task complete
+        truncated = self.steps >= self.max_steps  # episode ends when time limit
+        info = {}
+
+        self.state = next_state
+        return next_state, reward, terminated, truncated, info
+
+    def render(self, mode="human"):
+        # TODO, implement visualization
+        pass
