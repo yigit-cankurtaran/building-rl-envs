5bcdd145d9a75802064638c32506a0e0406957e8
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-15 14:31:23 +0300

    started the training code, wrote gitignore

--- gridworld_train.py
+++ gridworld_train.py
@@ -1,2 +1,15 @@
 from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import EvalCallback
 from gridworld_tut import GridWorldEnv
+
+# TODO: fill this out
+
+
+def train():
+    print("training should be here")
+
+
+if __name__ == "__main__":
+    train()
