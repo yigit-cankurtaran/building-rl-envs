5fd0b6dec15b56a0dcaf14dea4f5120a1f075a1c
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-15 14:36:53 +0300

    created envs, created eval callback

--- gridworld_train.py
+++ gridworld_train.py
@@ -1,14 +1,25 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.callbacks import EvalCallback
 from gridworld_tut import GridWorldEnv
-
-# TODO: fill this out
+import os
 
 
 def train():
-    print("training should be here")
+    train_env = make_vec_env(GridWorldEnv, n_envs=4)
+    eval_env = DummyVecEnv(GridWorldEnv)
+    # train and eval envs declared
+
+    os.makedirs("model", exist_ok=True)
+    os.makedirs("log", exist_ok=True)
+
+    model_path = "./model"
+    log_path = "./log"
+
+    # TODO: might need to ankify EvalCallback internals from previous training files
+    eval_callback = EvalCallback(eval_env, log_path, best_model_save_path=model_path)
 
 
 if __name__ == "__main__":
