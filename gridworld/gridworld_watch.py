783bceff206501eb7a494e3debce8df013c1b3ea
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-09-16 14:30:02 +0300

    implemented watching, doesn't render anything, will fix

--- gridworld/gridworld_watch.py
+++ gridworld/gridworld_watch.py
@@ -1,6 +1,19 @@
-import gymnasium as gym
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
+from gridworld_tut import GridWorldEnv
 
 
-# TODO: implement a way to watch your trained agent in the gridworld env
+def watch_trained_agent(ep_count=5):
+    model = PPO.load("./model/best_model.zip")
+    env = GridWorldEnv(render_mode="human")
+    rewards, lengths = evaluate_policy(
+        model, env, n_eval_episodes=ep_count, render=True, return_episode_rewards=True
+    )
+    for i in range(
+        len(rewards)
+    ):  # changed to len(rewards) to only access things that already exist
+        print(f"epoch:{i + 1}, reward:{rewards[i]}, length:{lengths[i]}")
+
+
+if __name__ == "__main__":
+    watch_trained_agent()
